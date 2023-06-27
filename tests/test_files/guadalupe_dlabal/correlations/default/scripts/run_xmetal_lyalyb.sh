c3976d7224ff06039344eba2e1a7b76b7bcc67d3
cesar ramirez <cramirez@ifae.es>
2023-06-27 04:22:34 -0700

    fix metal defaults

--- tests/test_files/guadalupe_dlabal/correlations/default/scripts/run_xmetal_lyalyb.sh
+++ tests/test_files/guadalupe_dlabal/correlations/default/scripts/run_xmetal_lyalyb.sh
@@ -16,5 +16,5 @@ export OMP_NUM_THREADS=128
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-command="picca_metal_xdmat.py --in-dir /global/u2/c/cramirez/Codes/picca_bookkeeper_new/tests/test_files/output/guadalupe/main/afterburn_v0/dMdB20_1_mgii_r.1.2_0/deltas/lyb/Delta --drq /global/u2/c/cramirez/Codes/picca_bookkeeper_new/tests/test_files/dummy_catalog-bal.fits --out /global/u2/c/cramirez/Codes/picca_bookkeeper_new/tests/test_files/output/guadalupe/main/afterburn_v0/dMdB20_1_mgii_r.1.2_0/correlations/default/correlations/lyalyalyb_qso/xmetal.fits.gz --lambda-abs LYA"
+command="picca_metal_xdmat.py --in-dir /global/u2/c/cramirez/Codes/picca_bookkeeper_new/tests/test_files/output/guadalupe/main/afterburn_v0/dMdB20_1_mgii_r.1.2_0/deltas/lyb/Delta --drq /global/u2/c/cramirez/Codes/picca_bookkeeper_new/tests/test_files/dummy_catalog-bal.fits --out /global/u2/c/cramirez/Codes/picca_bookkeeper_new/tests/test_files/output/guadalupe/main/afterburn_v0/dMdB20_1_mgii_r.1.2_0/correlations/default/correlations/lyalyalyb_qso/xmetal.fits.gz --lambda-abs LYA --mode desi_healpix --nproc 128 --rej 0.995 --abs-igm SiII(1260) SiIII(1207) SiII(1193) SiII(1190) --rp-min -300 --rp-max 300 --rt-max 200 --np 150 --nt 50 --fid-Or 7.975e-5"
 srun --nodes 1 --ntasks 1 --cpus-per-task 128 $command
