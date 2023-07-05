5e740c01da135ba588974e29713f7b534f089efe
cesar ramirez <cramirez@ifae.es>
2023-07-05 06:46:43 -0700

    changed raw survey

--- tests/test_files/raw/scripts/run_raw_deltas_lya.sh
+++ tests/test_files/raw/scripts/run_raw_deltas_lya.sh
@@ -6,8 +6,8 @@
 #SBATCH --constraint cpu
 #SBATCH --account desi
 #SBATCH --job-name raw_deltas_lya
-#SBATCH --output /picca_bookkeeper/tests/test_files/output/v9.0.0/raw/LyaCoLoRe/raw_0_0.0.0_0/logs/raw_deltas_lya-%j.out
-#SBATCH --error /picca_bookkeeper/tests/test_files/output/v9.0.0/raw/LyaCoLoRe/raw_0_0.0.0_0/logs/raw_deltas_lya-%j.err
+#SBATCH --output /picca_bookkeeper/tests/test_files/output/v9.0.0/desi-2.0-1000/LyaCoLoRe/raw_0_0.0.0_0/logs/raw_deltas_lya-%j.out
+#SBATCH --error /picca_bookkeeper/tests/test_files/output/v9.0.0/desi-2.0-1000/LyaCoLoRe/raw_0_0.0.0_0/logs/raw_deltas_lya-%j.err
 #SBATCH --mail-type fail
 #SBATCH --mail-user mail@host.es
 
@@ -17,5 +17,5 @@ umask 0002
 export OMP_NUM_THREADS=128
 
 
-command="picca_convert_transmission.py --object-cat /global/cfs/cdirs/desi/mocks/lya_forest/london/v9.0/v9.0.0/master.fits --in-dir /global/cfs/cdirs/desi/mocks/lya_forest/london/v9.0/v9.0.0 --out-dir /picca_bookkeeper/tests/test_files/output/v9.0.0/raw/LyaCoLoRe/raw_0_0.0.0_0/deltas/lya/Delta --lambda-rest-min 1040.0 --lambda-rest-max 1200.0 --linear-spacing True --lambda-min 3600 --lambda-max 5500 --delta-lambda 0.8"
+command="picca_convert_transmission.py --object-cat /global/cfs/cdirs/desi/mocks/lya_forest/develop/london/qq_desi/v9.0/v9.0.0/desi-2.0-1000/zcat.fits --in-dir /global/cfs/cdirs/desi/mocks/lya_forest/london/v9.0/v9.0.0 --out-dir /picca_bookkeeper/tests/test_files/output/v9.0.0/desi-2.0-1000/LyaCoLoRe/raw_0_0.0.0_0/deltas/lya/Delta --lambda-rest-min 1040.0 --lambda-rest-max 1200.0 --linear-spacing True --lambda-min 3600 --lambda-max 5500 --delta-lambda 0.8"
 srun --nodes 1 --ntasks 1 --cpus-per-task 128 $command
