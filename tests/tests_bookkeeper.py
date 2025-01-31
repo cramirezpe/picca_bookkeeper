5f2f324443af54036dfb2421eac6c15446057710
César Ramírez-Pérez <cramirpe@gmail.com>
2025-01-31 09:51:31 +0100

    feat(config): Improve bookkeeper config check

--- tests/tests_bookkeeper.py
+++ tests/tests_bookkeeper.py
@@ -852,6 +852,21 @@ class TestBookkeeper(unittest.TestCase):
         self.compare_bookkeeper_output(test_files, bookkeeper.paths.run_path)
 
     @patch("picca_bookkeeper.tasker.run", side_effect=mock_run)
+    def test_bad_config(self, mock_func_1):
+        copy_config_substitute(self.files_path / "example_bad_config.yaml")
+
+        with self.assertRaises(ValueError) as cm:
+            bookkeeper = Bookkeeper(
+                THIS_DIR / "test_files" / "output" / "tmp.yaml", read_mode=False
+            )
+            
+        self.assertEqual(
+            "Invalid region (nspec) in bookkeeper config section: delta extraction - "
+            "extra args - picca_convert_transmission",
+            str(cm.exception),
+        )
+
+    @patch("picca_bookkeeper.tasker.run", side_effect=mock_run)
     def test_example_custom_calib_raise_if_corrections_added(self, mock_func_1):
         copy_config_substitute(
             self.files_path / "example_config_custom_calib_with_calib_corrections.yaml"
